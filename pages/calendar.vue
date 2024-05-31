219abcae47745bb81784570487c9ce6d80e9e53c
jackal7819 <jackal3166@gmail.com>
2024-05-31 20:26:43 +0300

    Add newDates on Calendar

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -13,25 +13,56 @@
 		],
 	});
 
-	import { ref } from '#imports';
-	const date = ref(new Date());
-	const dates = ref([
-		new Date(2024, 4, 5),
-		new Date(2024, 4, 7),
-		new Date(2024, 4, 9),
-		new Date(2024, 4, 6),
-	]);
+	interface DateItem {
+		date: Date;
+		description: string;
+	}
 
-	const attributes = ref([
+	interface CalendarItem {
+		highlight: string;
+		dates: Date[];
+	}
+
+	const date = ref(new Date());
+	const selectedDates = ref<DateItem[]>([]);
+	const description = ref('');
+
+	const dateOptions: Intl.DateTimeFormatOptions = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	};
+
+	const attributes = ref<CalendarItem[]>([
 		{
 			highlight: 'green',
-			dates: new Date(),
-		},
-		{
-			highlight: 'blue',
-			dates,
+			dates: [new Date()],
 		},
 	]);
+
+	watch(
+		selectedDates,
+		(newDates) => {
+			attributes.value = [
+				...attributes.value,
+				{
+					highlight: 'blue',
+					dates: newDates.map((item) => item.date),
+				},
+			];
+		},
+		{ deep: true }
+	);
+
+	const addSelectedDate = () => {
+		selectedDates.value = [
+			...selectedDates.value,
+			{ date: date.value, description: description.value },
+		];
+		description.value = '';
+	};
 </script>
 
 <template>
@@ -44,8 +75,37 @@
 			is24hr
 			expanded
 		/>
-		<p class="mt-6 is-size-5 has-text-weight-semibold">
-			Selected Date: {{ date }}
-		</p>
+		<div class="mt-4 field">
+			<label class="label is-size-5">Description</label>
+			<div class="control">
+				<input
+					v-model="description"
+					class="input"
+					type="text"
+					placeholder="Enter description"
+				/>
+			</div>
+		</div>
+		<button class="mt-2 button is-primary" @click="addSelectedDate">
+			Add Event
+		</button>
+		<div v-if="selectedDates.length > 0">
+			<h3 class="mt-2 is-size-5 has-text-weight-semibold">EVENTS</h3>
+			<ul>
+				<li
+					v-for="selectedDate in selectedDates"
+					:key="selectedDate.toString()"
+					class="is-size-5 has-text-primary"
+				>
+					{{
+						selectedDate.date.toLocaleString(undefined, dateOptions)
+					}}
+					-
+					<span class="has-text-weight-semibold has-text-warning">{{
+						selectedDate.description
+					}}</span>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
